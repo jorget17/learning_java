f37cb91d53bf24fbfee8b57586bb07abebf51faf
JorgeT17 <jorget17@gmail.com>
2021-07-24 20:23:06 +0200

    pildoras variables

--- pildoras_java/proyecto001/src/HolaMundo.java
+++ pildoras_java/proyecto001/src/HolaMundo.java
@@ -2,7 +2,7 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.print("Hola mundo!!");
+		System.out.println("Hola mundo!!");
 
 	}
 
---
+++ pildoras_java/proyecto001/src/Variables.java
@@ -0,0 +1,16 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int num1;
+		int num2;
+		num1 = 4;
+		num2 = 8;
+		int suma = num1 + num2;
+		
+		System.out.println(suma);
+
+	}
+
+}
