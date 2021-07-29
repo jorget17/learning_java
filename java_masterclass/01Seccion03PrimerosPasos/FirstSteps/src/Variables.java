b10e9179623496f448c649a251981902ab2f0ce2
JorgeT17 <jorget17@gmail.com>
2021-07-29 16:22:36 +0200

    Variables

---
+++ java_masterclass/01Seccion03PrimerosPasos/FirstSteps/src/Variables.java
@@ -0,0 +1,39 @@
+public class Variables {
+
+    public static void main(String[] args){
+
+        // VARIABLES
+
+        /*
+
+        Variable --> una forma de acceder a la información en el ordenador.
+        Las variables que se definen en un programa pueden ser accedidas por su nombre y el ordenador
+        gestiona la forma en la que se almacenan en la memoria RAM.
+
+        El contenido de las variables puede cambiar a lo largo de la ejecución del programa.
+
+        Para definir una variable hay que decirle al ordenador qué tipo de datos va a contener y asignarle un nombre.
+
+        */
+
+        // Los datos pueden ser de varios tipos distintos (data types):
+
+        // INT --> números enteros
+
+        int myFirstNumber = 5; // declaration statement --> se indica el tipo de variable (int), el nombre y su valor (= 5)
+
+        System.out.println(myFirstNumber);
+
+        // Una expression es una estructura que evalúa a un valor único.
+
+        int expOne = (10+8) / 2;
+
+        System.out.println(expOne);
+
+        // STRING
+
+
+
+
+    }
+}
